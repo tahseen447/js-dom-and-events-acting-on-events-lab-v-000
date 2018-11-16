e8cee4a122e44667b2323d265e36cf0321d6678d
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-16 20:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function addNewElementAsLi(){
 function addNewLiOnClick(){
   let submit = document.querySelector('input[type="submit"]')
   submit.addEventListener('click', function(event){
-    addNewElementAsLi()
+    //addNewElementAsLi()
     resetInput()
   })
 }
