99f604e5fc39f0a4a15b23ad105e926fcfd74c23
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-16 20:19:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function retrieveEmployeeInformation(){
 function addNewElementAsLi(){
   let name = retrieveEmployeeInformation();
   let newElement = document.createElement('li');
-  newElement.createTextNode = name;
+  newElement.createTextNode(name);
   document.getElementById('.employee-list').appendChild(newElement);
 }
